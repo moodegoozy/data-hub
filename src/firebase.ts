0f4b0278ad19baa1e8efada2d642864656a60ac2
moodegoozy <moode434@gmail.com>
2025-12-09 11:41:52 +0100

    Integrate Firebase configuration

---
+++ src/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBMAZDUmMrEpuFCSKUYIHRh6NfAPdUV-z8',
+  authDomain: 'datahub-44154.firebaseapp.com',
+  projectId: 'datahub-44154',
+  storageBucket: 'datahub-44154.firebasestorage.app',
+  messagingSenderId: '923854285496',
+  appId: '1:923854285496:web:cdee6f15d3da5cf74e624d',
+};
+
+// Initialize Firebase once for the app lifecycle
+export const firebaseApp = initializeApp(firebaseConfig);
